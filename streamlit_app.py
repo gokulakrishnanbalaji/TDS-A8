5ee1e55f73cb223b0af9fb17f997efebc0aa8e31
Gokulakrishnan Balaji <gokulbalaji2408@gmail.com>
2023-04-19 15:03:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,9 +6,9 @@ def compute_max(a,b,c):
     l=list([a,b,c])
     return max(l)
 
-a= st.number_input("Enter the first number")
-b= st.number_input("Enter the second number")
-c= st.number_input("Enter the third number")
+a= st.number_input("Enter the first number",step=1)
+b= st.number_input("Enter the second number",step=1)
+c= st.number_input("Enter the third number",step=1)
 
 m=compute_max(a,b,c)
 st.write("Max = ",m)
